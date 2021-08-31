d8c6b78c5e970a2040a4e28d0a0d0e7bf5b47a6a
Erin Dawson <erinedawson@gmail.com>
2021-08-31 19:01:25 -0400

    include hero.php

--- resources/views/page-templates/landing-page-template.php
+++ resources/views/page-templates/landing-page-template.php
@@ -15,13 +15,16 @@ get_header(); ?>
 
 <div class="wit-landing-page container mx-auto p-4"> <!-- Start of .wit-landing-page -->
 
-	<?php while ( have_posts() ) : the_post(); ?>
-		<?php the_content() ?>
-	<?php endwhile;  ?>
+<!-- can be commented out,renders the editor content -->
+<!-- <?php while ( have_posts() ) : the_post(); ?> -->
+		<!-- <?php the_content() ?> -->
+	<!-- <?php endwhile;  ?> -->
 
 
     <section class="section-name mx-auto p-4 m-3.5">
        This an example of a section.
+
+       <?php include_once('service-page-modules/hero.php') ?>
     </section>
 
 </div> <!-- End of .wit-landing-page -->
