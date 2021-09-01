0a5972f29a1bc89c5876e1c67583aab04332faa1
Erin Dawson <erinedawson@gmail.com>
2021-09-01 14:58:12 -0400

    add phone number

--- resources/views/page-templates/service-page-modules/hero.php
+++ resources/views/page-templates/service-page-modules/hero.php
@@ -12,6 +12,18 @@ foreach ($parentFields as $field) {
 		} else {
 			$hero_title = '';
 		}
+
+		if( get_field('phone_number_tel') ) {
+			$phone_number_tel = get_field('phone_number_tel');
+		} else {
+			$phone_number_tel = '';
+		}
+
+		if( get_field('phone_number_display') ) {
+			$phone_number_display = get_field('phone_number_display');
+		} else {
+			$phone_number_display = '';
+		}
 ?>
 
 <div class="servicesHero hero pt-32 py-10 px-4 laptop:pt-40 laptop:pb-24" style="background: url( <?php echo $hero_background_image['url']; ?>) 100% 20% / cover;">
@@ -20,7 +32,9 @@ foreach ($parentFields as $field) {
 		<div class="servicesHeroSubtitle maxw450 laptop:px-3 mx-auto laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $hero_subtitle; ?></div>
 		<div class="servicesHeroTitle text-black text-34 laptop:text-52 leading-tight laptop:leading-none text-white"><?php echo $hero_title ?></div>
 		<div class="servicesHeroCall mt-3 laptop:mt-6 text-center text-white">
-			<?php echo do_shortcode ('[phone_button_alt class=""]'); ?>
+			<div class="heroPhone">
+				<a href="tel:1<?php echo $phone_number_tel ?>"><?php echo $phone_number_display ?></a>
+			</div>
 		</div>
 	</div>
 </div>
