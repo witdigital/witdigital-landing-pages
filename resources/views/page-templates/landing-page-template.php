aa87a7babd90e2e9b81e77aef4ed5e0fdbb23b94
Erin Dawson <erinedawson@gmail.com>
2021-09-01 15:51:58 -0400

    include cta

--- resources/views/page-templates/landing-page-template.php
+++ resources/views/page-templates/landing-page-template.php
@@ -23,6 +23,7 @@ get_header(); ?>
 <?php include_once('service-page-modules/hero.php') ?>
 <?php include_once('service-page-modules/intro.php') ?>
 <?php include_once('service-page-modules/about.php') ?>
+<?php include_once('service-page-modules/cta.php') ?>
    
 
 </div> <!-- End of .wit-landing-page -->
