28bf10b008324b6a1c75a507934e3f091ab54c11
Erin Dawson <erinedawson@gmail.com>
2021-09-03 13:40:57 -0400

    shortcode logic

--- resources/views/page-templates/service-page-modules/testimonials.php
+++ resources/views/page-templates/service-page-modules/testimonials.php
@@ -2,7 +2,6 @@
 
 $testimonials_background_image = (get_field('testimonials_background_image')) ? get_field('testimonials_background_image')['url'] : '' ;
 $testimonials_section_title = (get_field('testimonials_section_title')) ? get_field('testimonials_section_title') : '' ;
-$testimonials_shortcode = (get_field('testimonials_shortcode')) ? get_field('testimonials_shortcode') : '' ;
 
 ?>
 
@@ -11,8 +10,12 @@ $testimonials_shortcode = (get_field('testimonials_shortcode')) ? get_field('tes
 	<div class="testimonialContent max-w-1150 mx-auto px-4">
 		<h2 class="testimonialsTitle text-white text-center"><?php echo $testimonials_section_title ?></h2>
 		<div class="testimonialShortcode text-center my-10 text-white">
-			<?php echo do_shortcode ($testimonials_shortcode); ?>
-			<!-- <?php echo do_shortcode (get_field('testimonials_shortcode', 'option')); ?> -->
+			<?php if( get_field('testimonials_shortcode') ) {
+				echo do_shortcode (get_field('testimonials_shortcode'));
+			} else {
+				echo "TESTIMONIALS GO HERE.";
+			}
+			 ?>
 		</div>
 		<a class="ghostBtn btn centerBtn" href="/testimonials">READ REVIEWS</a>
 	</div>
