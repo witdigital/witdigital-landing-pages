1fa4c8733b43ec4415456f652814e7b2ea22dade
Erin Dawson <erinedawson@gmail.com>
2021-09-03 14:43:01 -0400

    about styling

--- resources/views/page-templates/service-page-modules/about.php
+++ resources/views/page-templates/service-page-modules/about.php
@@ -15,11 +15,11 @@ $services_about_right_content = (get_field('services_about_right_content')) ? ge
 ?>
 
 <div class="servicesAbout my-6 laptop:my-12">
-    <div class="servicesAboutInner max-w-1300 mx-auto laptop:px-4 flex flex-wrap justify-between">
-        <div class="servicesAboutLeft flex-1 max-w-650 px-4 laptop:px-0">
+    <div class="servicesAboutInner max-w-1300 w-full mx-auto laptop:px-4 flex flex-wrap justify-between">
+        <div class="servicesAboutLeft flex-1 max-w-650 w-full px-4 laptop:px-0">
             <?php echo $services_about_left ?>
         </div>
-        <div class="servicesAboutRight max-w-550 laptop:pl-5">
+        <div class="servicesAboutRight max-w-550 w-full laptop:pl-5">
             <div class="servicesAboutRight bg-blue rounded-2xl add-box-shadow p-5 laptop:py-8 laptop:px-10">
                 <h5 class="servicesRightSmallText text-lightblue mb-2 laptop:mb-4"><?php echo $services_about_right_title; ?></h5>
                 <h3 class="servicesRightLargeText text-white mb-6"><?php echo $services_about_right_subtitle; ?></h3>
