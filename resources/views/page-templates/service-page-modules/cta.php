319e1780992d2395396142160949071b431c882e
Erin Dawson <erinedawson@gmail.com>
2021-09-03 15:59:57 -0400

    full width

--- resources/views/page-templates/service-page-modules/cta.php
+++ resources/views/page-templates/service-page-modules/cta.php
@@ -13,11 +13,11 @@ $services_cta_title = (get_field('services_cta_title')) ? get_field('services_ct
 $services_cta_text = (get_field('services_cta_text')) ? get_field('services_cta_text') : '' ;
 ?>
 <div class="servicesCTA bg-grey">
-    <div class="servicesCTAInner max-w-1300 px-4 mx-auto py-8 laptop:flex flex-wrap justify-between ">
-        <div class="servicesCTALeft flex-1 max-w-600 laptop:pr-5">
+    <div class="servicesCTAInner max-w-1300 w-full px-4 mx-auto py-8 laptop:flex flex-wrap justify-between ">
+        <div class="servicesCTALeft flex-1 max-w-600 w-full laptop:pr-5">
             <img src="<?php echo $services_cta_image; ?>" alt="<?php echo $services_cta_image_alt; ?>" />
         </div>
-        <div class="servicesCTARight text-center max-w-650 laptop:pl-1">
+        <div class="servicesCTARight text-center max-w-650 w-full laptop:pl-1">
             <h3 class="servicesCTATitle"><?php echo $services_cta_title; ?></h3>
             <div class="servicesCTAText my-5">
                 <?php echo $services_cta_text; ?>
