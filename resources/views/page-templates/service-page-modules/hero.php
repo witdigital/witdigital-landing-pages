a802a8918d0510844dfe5eea20c3bf5376c49c8a
Erin Dawson <erinedawson@gmail.com>
2021-09-07 13:25:12 -0400

    display phone number

--- resources/views/page-templates/service-page-modules/hero.php
+++ resources/views/page-templates/service-page-modules/hero.php
@@ -11,6 +11,15 @@ foreach ($parentFields as $field) {
 $hero_title = (get_field('hero_title')) ? get_field('hero_title') : '' ;
 $phone_number_tel = (get_field('phone_number_tel')) ? get_field('phone_number_tel') : '' ;
 $phone_number_display = (get_field('phone_number_display')) ? get_field('phone_number_display') : '' ;
+
+$phone_1_href = (get_field('phone_1_href')) ? get_field('phone_1_href') : '' ;
+$phone_2_href = (get_field('phone_2_href')) ? get_field('phone_2_href') : '' ;
+$phone_1_display = (get_field('phone_1_display')) ? get_field('phone_1_display') : '' ;
+$phone_2_display = (get_field('phone_2_display')) ? get_field('phone_2_display') : '' ;
+$phone_1_location = (get_field('phone_1_location')) ? get_field('phone_1_location') : '' ;
+$phone_2_location = (get_field('phone_2_location')) ? get_field('phone_2_location') : '' ;
+$classes = (get_field('call_button_anchor_class')) ? get_field('call_button_anchor_class') : '' ;
+$call_button_icon = (get_field('call_button_icon')) ? get_field('call_button_icon')['url'] : '' ;
 ?>
 
 <div class="servicesHero hero pt-32 py-10 px-4 laptop:pt-40 laptop:pb-24" style="background: url( <?php echo $hero_background_image['url']; ?>) 100% 20% / cover;">
@@ -21,7 +30,19 @@ $phone_number_display = (get_field('phone_number_display')) ? get_field('phone_n
 		<div class="servicesHeroCall mt-3 laptop:mt-6 text-center text-white">
 			<div class="heroPhone">
 				<a href="tel:1<?php echo $phone_number_tel ?>"><?php echo $phone_number_display ?></a>
-				<?php echo do_shortcode('[witlandingpages_phone_button class=""]'); ?>
+				<!-- <?php echo do_shortcode('[witlandingpages_phone_button]'); ?> -->
+			</div>
+
+			<div class="phoneBtns">
+				<div class="phoneBtnItem">
+					<span class="phoneBtnItemLocation"><?php echo $phone_1_location?></span>
+					<a href="tel:<?php echo $phone_1_href?>" class="phoneBtn btn"><img class="iconImage phoneBtnImage align-middle" src="<?php echo $call_button_icon?>" /> <span class=""><?php echo $phone_1_display ?></span></a>
+				</div>
+				<!-- Uncomment the below if a second number is used: -->
+				<!-- <div class="phoneBtnItem">
+					<span class="phoneBtnItemLocation"><?php echo $phone_2_location?></span>
+					<a href="tel:<?php echo $phone_2_href?>" class="phoneBtn btn"><img class="iconImage phoneBtnImage align-middle" src="<?php echo $call_button_icon?>" /> <span class=""><?php echo $phone_2_display ?></span></a>
+				</div> -->
 			</div>
 		</div>
 	</div>
