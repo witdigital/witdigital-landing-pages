c92792b328a9ac5e5827afb2d3a1a1776282425a
Erin Dawson <erinedawson@gmail.com>
2021-09-07 12:53:25 -0400

    add phone_button shortcode

--- app/setup.php
+++ app/setup.php
@@ -38,3 +38,36 @@ if ( $template_path = 'landing-page-template.php' ) {
 	add_action( 'wp_enqueue_scripts', 'witlandingpages\custom_style_adding_function' );
 	add_action( 'admin_enqueue_scripts', 'witlandingpages\wit_landing_pages_enqueue_script' );
 }
+
+
+/* Shortcode for Phone Number: */
+add_shortcode('witlandingpages_phone_button', 'witlandingpages_phone_button');
+
+function witlandingpages_phone_button($atts) {
+	extract(shortcode_atts(array(
+		'class' => '',
+	), $atts));
+	// assign vars with logic:
+	$phone_1_href = (get_field('phone_1_href')) ? get_field('phone_1_href') : '' ;
+	$phone_2_href = (get_field('phone_2_href')) ? get_field('phone_2_href') : '' ;
+	$phone_1_display = (get_field('phone_1_display')) ? get_field('phone_1_display') : '' ;
+	$phone_2_display = (get_field('phone_2_display')) ? get_field('phone_2_display') : '' ;
+	$phone_1_location = (get_field('phone_1_location')) ? get_field('phone_1_location') : '' ;
+	$phone_2_location = (get_field('phone_2_location')) ? get_field('phone_2_location') : '' ;
+	$classes = (get_field('call_button_anchor_class')) ? get_field('call_button_anchor_class') : '' ;
+	$call_button_icon = (get_field('call_button_icon')) ? get_field('call_button_icon') : '' ;
+	
+
+	$phoneDisplay = '
+	<div class="phoneBtns">
+		<div class="phoneBtnItem">
+			<span class="phoneBtnItemLocation desktopOnly">' . $location1 . '</span>
+			<a href="tel:' . $href1 . '" class="phoneBtn btn ' . $class . ' ' . $classes . '"><img class="iconImage phoneBtnImage align-middle" src="' . $icon . '" /> <span class="desktopOnly text-blue">' . $display1 . '</span><span class="mobileOnly">' . $location1 . '</span></a>
+		</div>
+		<div class="phoneBtnItem">
+			<span class="phoneBtnItemLocation desktopOnly">' . $location2 . '</span>
+			<a href="tel:' . $href2 . '" class="phoneBtn btn ' . $class . ' ' . $classes . '"><img class="iconImage phoneBtnImage align-middle" src="' . $icon . '" /> <span class="desktopOnly text-blue">' . $display2 . '</span><span class="mobileOnly">' . $location2 . '</span></a>
+		</div>
+	</div>';
+	return $phoneDisplay;
+}
