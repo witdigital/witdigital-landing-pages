2f00af2d780e928df224b0bf4444c7c4856917dd
Erin Dawson <erinedawson@gmail.com>
2021-09-07 14:08:52 -0400

    sidebar menu

--- resources/views/page-templates/service-page-modules/about.php
+++ resources/views/page-templates/service-page-modules/about.php
@@ -24,24 +24,10 @@ $services_about_right_content = (get_field('services_about_right_content')) ? ge
                 <h5 class="servicesRightSmallText text-lightblue mb-2 laptop:mb-4"><?php echo $services_about_right_title; ?></h5>
                 <h3 class="servicesRightLargeText text-white mb-6"><?php echo $services_about_right_subtitle; ?></h3>
 
-                <ul class="servicesRightContentText">
-                <?php
+                <div class="servicesRightContentText">
+					<?php echo $services_about_right_content ?>
+                </div>
 
-global $post;
-
-if ( is_page() && $post->post_parent ) {
-    wp_list_pages(array(
-      'child_of' => $parent,
-      'title_li' => ''
-  ));
-} else {
-    wp_list_pages(array(
-      'child_of' => $post->ID,
-      'title_li' => ''
-  ));
-}
-?>
-				</ul>
             </div>
         </div>
     </div>
