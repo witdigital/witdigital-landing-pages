00dfc4d8a5294a9673b0160e8e610d6855636000
Erin Dawson <erinedawson@gmail.com>
2021-09-09 11:22:48 -0400

    font sizes

--- resources/views/page-templates/service-page-modules/content.php
+++ resources/views/page-templates/service-page-modules/content.php
@@ -52,7 +52,7 @@ foreach ($parentFields as $field) {
                             <?php
                             if(have_rows('services_sidebar_location_items')):
                                 while(have_rows('services_sidebar_location_items')):the_row(); ?>
-                                    <div class="locationItem text-white text-sm tracking-wide">
+                                    <div class="locationItem text-white text-base tracking-wide">
                                        <img class="iconImage" src=<?php echo $map_icon ?> /> <?php the_sub_field('location') ?>
                                     </div>
                                 <?php endwhile;
