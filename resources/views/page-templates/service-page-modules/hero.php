e2a2aeac73b1bae95d5155b02f6afcce36b35b1d
Erin Dawson <erinedawson@gmail.com>
2021-09-13 18:52:03 -0400

    housekeeping

--- resources/views/page-templates/service-page-modules/hero.php
+++ resources/views/page-templates/service-page-modules/hero.php
@@ -24,7 +24,7 @@ $call_button_icon = (get_field('call_button_icon')) ? get_field('call_button_ico
 	<div class="altOverlay overlay"></div>
 	<div class="servicesHeroInner max-w-1000 laptop:pb-16 laptop:py-0 text-center mx-auto laptop:pt-6">
 		<div class="servicesHeroSubtitle max-w-450 laptop:px-3 mx-auto laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $hero_subtitle; ?></div>
-		<div class="servicesHeroTitle text-black text-34 laptop:text-52 leading-tight laptop:leading-none text-white"><?php echo $hero_title ?></div>
+		<div class="servicesHeroTitle text-34 laptop:text-52 leading-tight laptop:leading-none text-white"><?php echo $hero_title ?></div>
 		<div class="servicesHeroCall mt-3 laptop:mt-6 text-center text-white">
 
 			<div class="phoneBtns">
