f8951bb17ac636aec5ffeabc0e09c03f6679bca8
Erin Dawson <erinedawson@gmail.com>
2021-09-15 16:18:00 -0400

    icon upload attempt

--- resources/assets/scripts/witdigital-landing-pages-scripts.js
+++ resources/assets/scripts/witdigital-landing-pages-scripts.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($){
 
     $('.accordionTitle').click(function() {
-        console.log('this is clicked');
 		$( this ).parent().toggleClass('active');
 	} );
+ 
 
     
 
