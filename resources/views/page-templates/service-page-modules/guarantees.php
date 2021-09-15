1e76577248a995756f17c673f1d702506d0d0318
Erin Dawson <erinedawson@gmail.com>
2021-09-15 13:18:31 -0400

    repeaters

--- resources/views/page-templates/service-page-modules/guarantees.php
+++ resources/views/page-templates/service-page-modules/guarantees.php
@@ -5,21 +5,7 @@ $guarantees_text = (get_field('witlandingpages_panel_section_text')) ? get_field
 $guarantees_section_image = (get_field('witlandingpages_p_desk')) ? get_field('witlandingpages_p_desk')['url'] : '' ;
 // if no mobile image selected, use desktop:
 $guarantees_mobile_image = (get_field('witlandingpages_p_mobile')) ? get_field('witlandingpages_p_mobile')['url'] : get_field('witlandingpages_p_desk')['url'] ;
-// Guarantee One
-$g_name_one = (get_field('g_name_one')) ? get_field('g_name_one') : '' ;
-$g_icon_one = (get_field('g_icon_one')) ? get_field('g_icon_one')['url'] : '' ;
-$g_icon_one_alt = (get_field('g_icon_one')) ? get_field('g_icon_one')['alt'] : '' ;
-$g_content_one = (get_field('g_content_one')) ? get_field('g_content_one') : '' ;
-// Guarantee Two
-$g_name_two = (get_field('g_name_two')) ? get_field('g_name_two') : '' ;
-$g_icon_two = (get_field('g_icon_two')) ? get_field('g_icon_two')['url'] : '' ;
-$g_icon_two_alt = (get_field('g_icon_two')) ? get_field('g_icon_two')['alt'] : '' ;
-$g_content_two = (get_field('g_content_two')) ? get_field('g_content_two') : '' ;
-// Guarantee Three
-$g_name_three = (get_field('g_name_three')) ? get_field('g_name_three') : '' ;
-$g_icon_three = (get_field('g_icon_three')) ? get_field('g_icon_three')['url'] : '' ;
-$g_icon_three_alt = (get_field('g_icon_three')) ? get_field('g_icon_three')['alt'] : '' ;
-$g_content_three = (get_field('g_content_three')) ? get_field('g_content_three') : '' ;
+
 
 ?>
 
@@ -30,33 +16,20 @@ $g_content_three = (get_field('g_content_three')) ? get_field('g_content_three')
 			<h2 class="pt-8 guaranteesTitle laptop:pt-0"><?php echo $guarantees_title; ?></h2>
 			<div class="mb-10 guaranteesText laptop:text-20"><?php echo $guarantees_text; ?></div>
 			<div class="flex-wrap justify-between guaranteesItems laptop:flex laptop:mb-10">
-				<div class="flex-1 px-4 guaranteeItem">
-					<div class="px-3 py-4 mb-4 text-center guaranteeItemInner bg-blue laptop:pt-6 laptop:pb-8 rounded-xl add-box-shadow laptop:mb-0">
-						<img class="mx-auto mb-3" src="<?php echo $g_icon_one; ?>" alt="<?php echo $g_icon_one_alt; ?>" />
-						<h4 class="mb-2 text-white guaranteeItemTitle laptop:mb-4"><?php echo $g_name_one; ?></h4>
-						<div class="text-white guaranteeItemText">
-							<?php echo $g_content_one; ?>
-						</div>
-					</div>
-				</div>
-				<div class="flex-1 px-4 guaranteeItem">
-					<div class="px-3 py-4 mb-4 text-center guaranteeItemInner bg-blue laptop:pt-6 laptop:pb-8 rounded-xl add-box-shadow laptop:mb-0">
-						<img class="mx-auto mb-3" src="<?php echo $g_icon_two; ?>" alt="<?php echo $g_icon_two_alt; ?>" />
-						<h4 class="mb-2 text-white guaranteeItemTitle laptop:mb-4"><?php echo $g_name_two; ?></h4>
-						<div class="text-white guaranteeItemText">
-							<?php echo $g_content_two; ?>
-						</div>
-					</div>
-				</div>
-				<div class="flex-1 px-4 guaranteeItem">
-					<div class="px-3 py-4 mb-4 text-center guaranteeItemInner bg-blue laptop:pt-6 laptop:pb-8 rounded-xl add-box-shadow laptop:mb-0">
-						<img class="mx-auto mb-3" src="<?php echo $g_icon_three; ?>" alt="<?php echo $g_icon_three_alt; ?>" />
-						<h4 class="mb-2 text-white guaranteeItemTitle laptop:mb-4"><?php echo $g_name_three; ?></h4>
-						<div class="text-white guaranteeItemText">
-							<?php echo $g_content_three; ?>
-						</div>
-					</div>
-				</div>
+				<?php if( have_rows('witlandingpages_panels') ):
+						while( have_rows('witlandingpages_panels') ): the_row(); ?>
+							<div class="flex-1 px-4 guaranteeItem">
+
+								<div class="px-3 py-4 mb-4 text-center guaranteeItemInner bg-blue laptop:pt-6 laptop:pb-8 rounded-xl add-box-shadow laptop:mb-0">
+									<img class="mx-auto mb-3" src="<?php the_sub_field('witlandingpages_p_image')['url']; ?>" alt="<?php the_sub_field('witlandingpages_p_image')['alt']; ?>" />
+									<h4 class="mb-2 text-white guaranteeItemTitle laptop:mb-4"><?php the_sub_field('witlandingpages_p_title'); ?></h4>
+									<div class="text-white guaranteeItemText"><?php the_sub_field('witlandingpages_p_text'); ?></div>
+								</div>
+
+							</div>
+						<?php endwhile; 
+					endif;
+				?>
 			</div>
 			<div class="mb-3 laptop:mb-0">
 
