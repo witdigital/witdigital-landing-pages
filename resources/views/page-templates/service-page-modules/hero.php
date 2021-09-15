d17b3e070597ea6c6b8f3dd843edf0636ecf6a29
Erin Dawson <erinedawson@gmail.com>
2021-09-15 16:37:35 -0400

    add default phone

--- resources/views/page-templates/service-page-modules/hero.php
+++ resources/views/page-templates/service-page-modules/hero.php
@@ -18,26 +18,26 @@ $phone_1_display = (get_field('witlandingpages_phone_format')) ? get_field('witl
 $phone_2_display = (get_field('witlandingpages_phone_2_format')) ? get_field('witlandingpages_phone_2_format') : '' ;
 $phone_1_location = (get_field('witlandingpages_phone_location')) ? get_field('witlandingpages_phone_location') : '' ;
 $phone_2_location = (get_field('witlandingpages_phone_2_location')) ? get_field('witlandingpages_phone_2_location') : '' ;
-$call_button_icon = (get_field('call_button_icon')) ? get_field('call_button_icon')['url'] : '' ;
+$call_button_icon = (get_field('call_button_icon')) ? get_field('call_button_icon')['url'] : '/wp-content/plugins/witdigital-landing-pages/resources/assets/images/phone-alt-solid.svg" /' ;
 ?>
 
-<div class="servicesHero hero pt-32 py-10 px-4 laptop:pt-40 laptop:pb-24" style="background: url( <?php echo $witlandingpages_hero_image['url']; ?>) 100% 20% / cover;">
+<div class="px-4 py-10 pt-32 servicesHero hero laptop:pt-40 laptop:pb-24" style="background: url( <?php echo $witlandingpages_hero_image['url']; ?>) 100% 20% / cover;">
 	<div class="altOverlay overlay"></div>
-	<div class="servicesHeroInner max-w-1000 laptop:pb-16 laptop:py-0 text-center mx-auto laptop:pt-6">
-		<div class="servicesHeroSubtitle max-w-450 laptop:px-3 mx-auto laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $witlandingpages_hero_subtitle; ?></div>
-		<div class="servicesHeroTitle text-34 laptop:text-52 leading-tight laptop:leading-none text-white"><?php echo $hero_title ?></div>
-		<div class="servicesHeroCall mt-3 laptop:mt-6 text-center text-white">
+	<div class="mx-auto text-center servicesHeroInner max-w-1000 laptop:pb-16 laptop:py-0 laptop:pt-6">
+		<div class="mx-auto servicesHeroSubtitle max-w-450 laptop:px-3 laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $witlandingpages_hero_subtitle; ?></div>
+		<div class="leading-tight text-white servicesHeroTitle text-34 laptop:text-52 laptop:leading-none"><?php echo $hero_title ?></div>
+		<div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
 
-			<div class="phoneBtns flex flex-nowrap justify-between items-center my-0 mx-auto max-w-340">
+			<div class="flex items-center justify-between mx-auto my-0 phoneBtns flex-nowrap max-w-340">
 				<div class="phoneBtnItem mb-2.5">
 					<span class="phoneBtnItemLocation block mb-1.5 font-bold"><?php echo $phone_1_location?></span>
-					<a href="tel:<?php echo $phone_1_href?>" class="phoneBtn btn bg-white rounded-md py-2 px-3 text-base font-bold"><img class="iconImage phoneBtnImage align-middle" src="<?php echo $call_button_icon?>" /> <span class=""><?php echo $phone_1_display ?></span></a>
+					<a href="tel:<?php echo $phone_1_href?>" class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="align-middle iconImage phoneBtnImage" src="<?php echo $call_button_icon?>" /> <span class=""><?php echo $phone_1_display ?></span></a>
 				</div>
 
 				<?php if( ($phone_2_href) && ($phone_2_display) ): ?>
 				<div class="phoneBtnItem mb-2.5">
 					<span class="phoneBtnItemLocation block mb-1.5 font-bold"><?php echo $phone_2_location?></span>
-					<a href="tel:<?php echo $phone_2_href?>" class="phoneBtn btn bg-white rounded-md py-2 px-3 text-base font-bold"><img class="iconImage phoneBtnImage align-middle" src="<?php echo $call_button_icon?>" /> <span class=""><?php echo $phone_2_display ?></span></a>
+					<a href="tel:<?php echo $phone_2_href?>" class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="align-middle iconImage phoneBtnImage" src="<?php echo $call_button_icon?>" /> <span class=""><?php echo $phone_2_display ?></span></a>
 				</div>
 				<?php endif; ?>
 
