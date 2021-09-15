be3879740c5784afd79f26ccf7ad2f8d6ef59571
Erin Dawson <erinedawson@gmail.com>
2021-09-15 16:35:03 -0400

    replace fields

--- resources/views/page-templates/service-page-modules/testimonials.php
+++ resources/views/page-templates/service-page-modules/testimonials.php
@@ -1,22 +1,24 @@
 <?php
 
-$testimonials_background_image = (get_field('testimonials_background_image')) ? get_field('testimonials_background_image')['url'] : '' ;
-$testimonials_section_title = (get_field('testimonials_section_title')) ? get_field('testimonials_section_title') : '' ;
+$wpl_testimonials_img = (get_field('witlandingpages_testimonials_bkgrnd')) ? get_field('witlandingpages_testimonials_bkgrnd')['url'] : '' ;
+$wpl_testimonials_title = (get_field('witlandingpages_testimonials_title')) ? get_field('witlandingpages_testimonials_title') : '' ;
+$wpl_testBtn_href = (get_field('witlandingpages_testimonials_link')) ? get_field('witlandingpages_testimonials_link') : '' ;
+$wpl_testBtn_text = (get_field('witlandingpages_testimonials_btn_text')) ? get_field('witlandingpages_testimonials_btn_text') : '' ;
 
 ?>
 
-<div class="testimonialsInner py-12 laptop:mt-48" style="background: url(<?php echo $testimonials_background_image ?>) center / cover;">
+<div class="py-12 testimonialsInner laptop:mt-48" style="background: url(<?php echo $wpl_testimonials_img ?>) center / cover;">
 	<div class="overlay altOverlay"></div>
-	<div class="testimonialContent max-w-1150 mx-auto px-4">
-		<h2 class="testimonialsTitle text-white text-center"><?php echo $testimonials_section_title ?></h2>
-		<div class="testimonialShortcode text-center my-10 text-white">
-			<?php if( get_field('testimonials_shortcode') ) {
-				echo do_shortcode (get_field('testimonials_shortcode'));
+	<div class="px-4 mx-auto testimonialContent max-w-1150">
+		<h2 class="text-center text-white testimonialsTitle"><?php echo $wpl_testimonials_title ?></h2>
+		<div class="my-10 text-center text-white testimonialShortcode">
+			<?php if( get_field('witlandingpages_testimonials_shortcode') ) {
+				echo do_shortcode (get_field('witlandingpages_testimonials_shortcode'));
 			} else {
 				echo "TESTIMONIALS GO HERE.";
 			}
 			 ?>
 		</div>
-		<a class="btn py-2 px-3 my-0 mx-auto block text-white border-white border rounded-md text-center text-base uppercase font-bold leading-none" href="/testimonials">READ REVIEWS</a>
+		<a class="block px-3 py-2 mx-auto my-0 text-base font-bold leading-none text-center text-white uppercase border border-white rounded-md btn" href="<?php echo $wpl_testBtn_href ?>"><?php echo $wpl_testBtn_text ?></a>
 	</div>
 </div>
