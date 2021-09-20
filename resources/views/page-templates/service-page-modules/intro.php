2bf113f36bd4f0ed4ae0050c8fed4658404e424a
Erin Dawson <erinedawson@gmail.com>
2021-09-20 18:29:46 -0400

    border-radius

--- resources/views/page-templates/service-page-modules/intro.php
+++ resources/views/page-templates/service-page-modules/intro.php
@@ -2,11 +2,21 @@
 <?php
 $wlp_intro_embed = (get_field('witlandingpages_intro_embed')) ? get_field('witlandingpages_intro_embed') : '' ;
 $wlp_intro_image = (get_field('witlandingpages_intro_image')) ? get_field('witlandingpages_intro_image')['url'] : '' ;
+$wlp_intro_b_rad = (get_field('witlandingpages_intro_border_rad')) ? get_field('witlandingpages_intro_border_rad') : '' ;
 $wlp_intro_title = (get_field('witlandingpages_intro_title')) ? get_field('witlandingpages_intro_title') : '' ;
 $wlp_intro_text = (get_field('witlandingpages_intro_text')) ? get_field('witlandingpages_intro_text') : '' ;
 $wlp_request_url = (get_field('witlandingpages_request_url')) ? get_field('witlandingpages_request_url') : '' ;
 $wlp_request_display = (get_field('witlandingpages_request_display')) ? get_field('witlandingpages_request_display') : '' ;
+
+
+$wlp_intro_radius = "border-radius: " . $wlp_intro_b_rad . "rem;";
 ?>
+
+<style>
+    .servicesIntroImage {
+        <?php echo $wlp_intro_radius ?>
+    }
+</style>
 
 <div class="my-5 servicesIntro laptop:mt-12 laptop:mb-16">
     <div class="servicesIntroInner mx-auto px-4 flex flex-wrap justify-between items-center <?php echo get_field('witlandingpages_image_video') == 'Embed' ? 'embed' : 'image'; ?>">
