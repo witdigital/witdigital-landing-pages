bea8e6077b780bac2dd6f7720cae27dc3386298d
Erin Dawson <erinedawson@gmail.com>
2021-09-27 15:32:28 -0400

    update phone display

--- app/shortcode.php
+++ app/shortcode.php
@@ -21,14 +21,10 @@ function witlandingpages_phone_button( $atts ) {
     $wlp_call_button_icon = (get_field('witlandingpages_phone_icon')) ? get_field('witlandingpages_phone_icon')['url'] : '/wp-content/plugins/witdigital-landing-pages/resources/assets/images/phone-alt-solid.svg" /' ;
 
     $wlp_phonedisplay_div = 
-        '<div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
-
-        <div class="flex items-center justify-center mx-auto my-0 phoneBtns flex-nowrap max-w-340">
+        '<div class="flex items-center justify-center mx-auto my-0 phoneBtns flex-nowrap max-w-340">
             <div class="phoneBtnItem mb-2.5">
                 <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_1_location . '</span>
                 <a href="tel:' . $wlp_phone_1_href . ' " class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="inline-block mr-1 w-3.5 align-middle iconImageFilter" src="' . $wlp_call_button_icon . ' " /> <span class="">' . $wlp_phone_1_display . '</span></a>
-            </div>
-
             </div>
 
         </div>' ;
@@ -56,9 +52,7 @@ function witlandingpages_twophones_button( $atts ) {
     $wlp_call_button_icon = (get_field('witlandingpages_phone_icon')) ? get_field('witlandingpages_phone_icon')['url'] : '/wp-content/plugins/witdigital-landing-pages/resources/assets/images/phone-alt-solid.svg" /' ;
 
     $wlp_twophonedisplay_div = 
-        '<div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
-
-        <div class="flex items-center justify-between mx-auto my-0 phoneBtns flex-nowrap max-w-340">
+        '<div class="flex items-center justify-between mx-auto my-0 phoneBtns flex-nowrap max-w-340">
             <div class="phoneBtnItem mb-2.5">
                 <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_1_location . '</span>
                 <a href="tel:' . $wlp_phone_1_href . ' " class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="inline-block mr-1 w-3.5 align-middle iconImageFilter" src="' . $wlp_call_button_icon . ' " /> <span class="">' . $wlp_phone_1_display . '</span></a>
@@ -68,8 +62,6 @@ function witlandingpages_twophones_button( $atts ) {
                 <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_2_location . '</span>
                 <a href="tel:' . $wlp_phone_2_href . ' " class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="inline-block
                 mr-1 w-3.5 align-middle iconImageFilter" src=" ' . $wlp_call_button_icon . ' " /> <span class=""> ' . $wlp_phone_2_display  . '</span></a>
-            </div>
-
             </div>
 
         </div>' ;
--- resources/views/page-templates/service-page-modules/hero.php
+++ resources/views/page-templates/service-page-modules/hero.php
@@ -48,6 +48,10 @@ $wlp_gradient = "background: " . $wlp_overlay_gradient . '!important;';
 	<div class="mx-auto text-center servicesHeroInner max-w-1000 laptop:pb-16 laptop:py-0 laptop:pt-6">
 		<span class="mx-auto servicesHeroSubtitle max-w-450 laptop:px-3 laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $witlandingpages_hero_subtitle; ?></span>
 		<h1 class="leading-tight text-white servicesHeroTitle text-34 laptop:text-52 laptop:leading-none"><?php echo $wlp_hero_title ?></h1>
+
+		<div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
 			<?php echo $wlp_hero_sc ?>
+		</div>
+		
 	</div>
 </div>
