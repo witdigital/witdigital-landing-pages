43e347bf8134606ef17aea0c9a00c2fd23786358
Erin Dawson <erinedawson@gmail.com>
2021-09-27 15:36:39 -0400

    add shortcode

--- resources/views/page-templates/service-page-modules/cta.php
+++ resources/views/page-templates/service-page-modules/cta.php
@@ -11,6 +11,7 @@ $witlandingpages_cta_image = (get_field('witlandingpages_cta_image')) ? get_fiel
 $services_cta_image_alt = (get_field('witlandingpages_cta_image')) ? get_field('witlandingpages_cta_image')['alt'] : '' ;
 $witlandingpages_cta_title = (get_field('witlandingpages_cta_title')) ? get_field('witlandingpages_cta_title') : '' ;
 $witlandingpages_cta_content = (get_field('witlandingpages_cta_content')) ? get_field('witlandingpages_cta_content') : '' ;
+$wlp_cta_sc = (get_field('witlandingpages_cta_shortcode')) ? do_shortcode (get_field('witlandingpages_cta_shortcode')) : '' ;
 
 $wlp_cta_background_class = (get_field('witlandingpages_cta_background')) == 'Color' ? 'ctaColorOverlay' : 'ctaGradientOverlay' ;
 $wlp_cta_background_color = (get_field('witlandingpages_cta_background_color')) ? get_field('witlandingpages_cta_background_color') : '' ;
@@ -39,21 +40,8 @@ $wlp_cta_gradient = "background: " . $wlp_cta_background_gradient . '!important;
             <div class="my-5 servicesCTAText">
                 <?php echo $witlandingpages_cta_content; ?>
             </div>
- 
-			<div class="flex items-center justify-between mx-auto my-0 phoneBtns flex-nowrap max-w-340">
-				<div class="phoneBtnItem mb-2.5">
-					<span class="phoneBtnItemLocation block mb-1.5 font-bold"><?php echo $wlp_phone_1_location?></span>
-					<a href="tel:<?php echo $wlp_phone_1_href?>" class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="align-middle iconImage phoneBtnImage w-3.5" src="<?php echo $wlp_call_button_icon?>" /> <span class=""><?php echo $wlp_phone_1_display ?></span></a>
-				</div>
-
-				<?php if( ($wlp_phone_2_href) && ($wlp_phone_2_display) ): ?>
-				<div class="phoneBtnItem mb-2.5">
-					<span class="phoneBtnItemLocation block mb-1.5 font-bold"><?php echo $wlp_phone_2_location?></span>
-					<a href="tel:<?php echo $wlp_phone_2_href?>" class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="align-middle iconImage phoneBtnImage w-3.5" src="<?php echo $wlp_call_button_icon?>" /> <span class=""><?php echo $wlp_phone_2_display ?></span></a>
-				</div>
-				<?php endif; ?>
-
-			</div> <!-- end .phoneBtns -->
+	
+			<?php echo $wlp_cta_sc ?>
             
         </div>
     </div>
