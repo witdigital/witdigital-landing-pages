64f9d59529a366e1d992f64c7f9a9769df347731
Erin Dawson <erinedawson@gmail.com>
2021-09-27 14:21:15 -0400

    phone shortcodes

--- app/shortcode.php
+++ app/shortcode.php
@@ -3,7 +3,7 @@
 // Shortcodes for phone button and Request Service button
 
 
-// Phone Button:
+// Phone Button (with ONE phone number):
 
 function witlandingpages_phone_button( $atts ) {
     extract(shortcode_atts(array(
@@ -23,6 +23,41 @@ function witlandingpages_phone_button( $atts ) {
     $wlp_phonedisplay_div = 
         '<div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
 
+        <div class="flex items-center justify-center mx-auto my-0 phoneBtns flex-nowrap max-w-340">
+            <div class="phoneBtnItem mb-2.5">
+                <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_1_location . '</span>
+                <a href="tel:' . $wlp_phone_1_href . ' " class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="inline-block mr-1 w-3.5 align-middle iconImageFilter" src="' . $wlp_call_button_icon . ' " /> <span class="">' . $wlp_phone_1_display . '</span></a>
+            </div>
+
+            </div>
+
+        </div>' ;
+
+        return $wlp_phonedisplay_div;
+};
+add_shortcode('witlandingpages_phone_button', 'witlandingpages_phone_button');
+
+
+// Phone Button (with TWO phone numbers):
+
+function witlandingpages_twophones_button( $atts ) {
+    extract(shortcode_atts(array(
+        'class' => '',
+      ), $atts));
+
+    // declare phone button variables:
+
+    $wlp_phone_1_href = (get_field('witlandingpages_phone_link')) ? get_field('witlandingpages_phone_link') : '' ;
+    $wlp_phone_2_href = (get_field('witlandingpages_phone_2_link')) ? get_field('witlandingpages_phone_2_link') : '' ;
+    $wlp_phone_1_display = (get_field('witlandingpages_phone_format')) ? get_field('witlandingpages_phone_format') : '' ;
+    $wlp_phone_2_display = (get_field('witlandingpages_phone_2_format')) ? get_field('witlandingpages_phone_2_format') : '' ;
+    $wlp_phone_1_location = (get_field('witlandingpages_phone_location')) ? get_field('witlandingpages_phone_location') : '' ;
+    $wlp_phone_2_location = (get_field('witlandingpages_phone_2_location')) ? get_field('witlandingpages_phone_2_location') : '' ;
+    $wlp_call_button_icon = (get_field('witlandingpages_phone_icon')) ? get_field('witlandingpages_phone_icon')['url'] : '/wp-content/plugins/witdigital-landing-pages/resources/assets/images/phone-alt-solid.svg" /' ;
+
+    $wlp_twophonedisplay_div = 
+        '<div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
+
         <div class="flex items-center justify-between mx-auto my-0 phoneBtns flex-nowrap max-w-340">
             <div class="phoneBtnItem mb-2.5">
                 <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_1_location . '</span>
@@ -34,15 +69,14 @@ function witlandingpages_phone_button( $atts ) {
                 <a href="tel:' . $wlp_phone_2_href . ' " class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="inline-block
                 mr-1 w-3.5 align-middle iconImageFilter" src=" ' . $wlp_call_button_icon . ' " /> <span class=""> ' . $wlp_phone_2_display  . '</span></a>
             </div>
-         
 
             </div>
 
         </div>' ;
 
-        return $wlp_phonedisplay_div;
+        return $wlp_twophonedisplay_div;
 };
-add_shortcode('witlandingpages_phone_button', 'witlandingpages_phone_button');
+add_shortcode('witlandingpages_twophones_button', 'witlandingpages_twophones_button');
 
 
 
