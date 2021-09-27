2425b722f1df474e1d7f67573a85b8b081072a3c
Erin Dawson <erinedawson@gmail.com>
2021-09-27 14:58:21 -0400

    refactor

--- resources/views/page-templates/service-page-modules/testimonials.php
+++ resources/views/page-templates/service-page-modules/testimonials.php
@@ -4,6 +4,7 @@ $wlp_testimonials_img = (get_field('witlandingpages_testimonials_bkgrnd')) ? get
 $wlp_testimonials_title = (get_field('witlandingpages_testimonials_title')) ? get_field('witlandingpages_testimonials_title') : '' ;
 $wlp_testBtn_href = (get_field('witlandingpages_testimonials_link')) ? get_field('witlandingpages_testimonials_link') : '' ;
 $wlp_testBtn_text = (get_field('witlandingpages_testimonials_btn_text')) ? get_field('witlandingpages_testimonials_btn_text') : '' ;
+$wlp_testimonials_sc = (get_field('witlandingpages_testimonials_shortcode')) ? do_shortcode (get_field('witlandingpages_testimonials_shortcode')) : 'words GO HERE.' ;
 
 $wlp_test_overlay_class = (get_field('witlandingpages_overlay_testimonials')) == 'Color' ? 'testColorOverlay' : 'testGradientOverlay' ;
 $wlp_test_overlay_color = (get_field('witlandingpages_overlay_color_testimonials')) ? get_field('witlandingpages_overlay_color_testimonials') : '' ;
@@ -28,12 +29,7 @@ $wlp_styles_test_gradient = "background: " . $wlp_test_overlay_gradient . '!impo
 	<div class="px-4 mx-auto testimonialContent max-w-1150">
 		<h2 class="text-center text-white testimonialsTitle"><?php echo $wlp_testimonials_title ?></h2>
 		<div class="my-10 text-center text-white testimonialShortcode">
-			<?php if( get_field('witlandingpages_testimonials_shortcode') ) {
-				echo do_shortcode (get_field('witlandingpages_testimonials_shortcode'));
-			} else {
-				echo "TESTIMONIALS GO HERE.";
-			}
-			 ?>
+			 <?php echo $wlp_testimonials_sc ?>
 		</div>
 		<a class="block px-3 py-2 mx-auto my-0 text-base font-bold leading-none text-center text-white uppercase border border-white rounded-md btn focus:bg-white focus:text-blue" href="<?php echo $wlp_testBtn_href ?>"><?php echo $wlp_testBtn_text ?></a>
 	</div>
