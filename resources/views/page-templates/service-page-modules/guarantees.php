b62ffb875eed579a5dc2962720fbc05c5b848990
Erin Dawson <erinedawson@gmail.com>
2021-09-27 15:40:26 -0400

    add shortcode

--- resources/views/page-templates/service-page-modules/guarantees.php
+++ resources/views/page-templates/service-page-modules/guarantees.php
@@ -5,6 +5,7 @@ $wlp_panels_section_img = (get_field('witlandingpages_p_desk')) ? get_field('wit
 // if no mobile image selected, use desktop:
 $wlp_panels_mobile_img = (get_field('witlandingpages_p_mobile')) ? get_field('witlandingpages_p_mobile')['url'] : get_field('witlandingpages_p_desk')['url'] ;
 $wlp_panels_rad_desk_img = (get_field('witlandingpages_pdesk_border_radius')) ? get_field('witlandingpages_pdesk_border_radius') : '' ;
+$wlp_panels_sc = (get_field('witlandingpages_panels_shortcode')) ? do_shortcode (get_field('witlandingpages_panels_shortcode')) : '' ;
 
 if( have_rows('witlandingpages_panel_styles') ):
     while( have_rows('witlandingpages_panel_styles') ): the_row();
@@ -73,21 +74,7 @@ $wlp_styles_rad_desk_img = "border-radius: " . $wlp_panels_rad_desk_img . "rem;"
 			</div>
 			<div class="mb-3 laptop:mb-0">
 
-
-			<div class="flex items-center justify-between mx-auto my-0 phoneBtns flex-nowrap max-w-340">
-				<div class="phoneBtnItem mb-2.5">
-					<span class="phoneBtnItemLocation block mb-1.5 font-bold"><?php echo $wlp_phone_1_location?></span>
-					<a href="tel:<?php echo $wlp_phone_1_href?>" class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="align-middle iconImage phoneBtnImage w-3.5" src="<?php echo $wlp_call_button_icon?>" /> <span class=""><?php echo $wlp_phone_1_display ?></span></a>
-				</div>
-
-				<?php if( ($wlp_phone_2_href) && ($wlp_phone_2_display) ): ?>
-				<div class="phoneBtnItem mb-2.5">
-					<span class="phoneBtnItemLocation block mb-1.5 font-bold"><?php echo $wlp_phone_2_location?></span>
-					<a href="tel:<?php echo $wlp_phone_2_href?>" class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="align-middle iconImage phoneBtnImage w-3.5" src="<?php echo $wlp_call_button_icon?>" /> <span class=""><?php echo $wlp_phone_2_display ?></span></a>
-				</div>
-				<?php endif; ?>
-
-			</div> <!-- end .phoneBtns -->
+				<?php echo $wlp_panels_sc ?>
 
 			</div>
 		</div>
