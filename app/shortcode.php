062607816398607483dec016fdd782a06f7ba3c0
Erin Dawson <erinedawson@gmail.com>
2021-09-27 17:01:52 -0400

    add calendar icon

--- app/shortcode.php
+++ app/shortcode.php
@@ -20,8 +20,12 @@ function witlandingpages_phone_button( $atts ) {
     $wlp_phone_2_location = (get_field('witlandingpages_phone_2_location')) ? get_field('witlandingpages_phone_2_location') : '' ;
     $wlp_call_button_icon = (get_field('witlandingpages_phone_icon')) ? get_field('witlandingpages_phone_icon')['url'] : '/wp-content/plugins/witdigital-landing-pages/resources/assets/images/phone-alt-solid.svg" /' ;
 
+
     $wlp_phonedisplay_div = 
-        '<div class="flex items-center justify-center mx-auto my-0 phoneBtns flex-nowrap max-w-340">
+        '<style>
+        .phoneBtnItemLocation {color: red;}
+        </style>
+        <div class="flex items-center justify-center mx-auto my-0 phoneBtns flex-nowrap max-w-340">
             <div class="phoneBtnItem mb-2.5">
                 <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_1_location . '</span>
                 <a href="tel:' . $wlp_phone_1_href . ' " class="px-3 py-2 text-base font-bold bg-white rounded-md phoneBtn btn"><img class="inline-block mr-1 w-3.5 align-middle iconImageFilter" src="' . $wlp_call_button_icon . ' " /> <span class="">' . $wlp_phone_1_display . '</span></a>
