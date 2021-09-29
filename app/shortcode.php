31cd6b2d3b0a86be43c4d3cc4a2cd587d94a1f28
Erin Dawson <erinedawson@gmail.com>
2021-09-29 15:20:59 -0400

    default_phone

--- app/shortcode.php
+++ app/shortcode.php
@@ -3,9 +3,9 @@
 // Shortcodes for phone button and Request Service button
 
 
-// Phone Button (with ONE phone number):
+// Phone Button (with ONE phone number and default icon):
 
-function witlandingpages_phone_button( $atts ) {
+function witlandingpages_default_phone( $atts ) {
     extract(shortcode_atts(array(
         'class' => '',
       ), $atts));
@@ -14,56 +14,45 @@ function witlandingpages_phone_button( $atts ) {
 
     $wlp_phone_1_href = (get_field('witlandingpages_phone_link')) ? get_field('witlandingpages_phone_link') : '' ;
     $wlp_phone_1_display = (get_field('witlandingpages_phone_format')) ? get_field('witlandingpages_phone_format') : '' ;
-    $wlp_phone_1_location = (get_field('witlandingpages_phone_location')) ? get_field('witlandingpages_phone_location') : '' ;
     $wlp_call_button_icon = '/wp-content/plugins/witdigital-landing-pages/resources/assets/images/phone-alt-solid.svg" /' ;
 
        // Style Variables:
-       if( have_rows('witlandingpages_phone_styling') ) :
-        while( have_rows('witlandingpages_phone_styling') ): the_row();
+       if( have_rows('witlandingpages_phone_styling', 'option') ) :
+        while( have_rows('witlandingpages_phone_styling', 'option') ): the_row();
     
-        $wlp_phone_background = (get_sub_field('witlandingpages_phone_background_color')) ? get_sub_field('witlandingpages_phone_background_color') : '' ;
-        $wlp_phone_border_radius = (get_sub_field('witlandingpages_phone_border_radius')) ? get_sub_field('witlandingpages_phone_border_radius') : '' ;
-        $wlp_phone_location_color = (get_sub_field('witlandingpages_phone_location_color')) ? get_sub_field('witlandingpages_phone_location_color') : '' ;
-        $wlp_phone_body_color = (get_sub_field('witlandingpages_phone_body_color')) ? get_sub_field('witlandingpages_phone_body_color') : '' ;
+        $wlp_phone_background = (get_sub_field('witlandingpages_phone_background_color')) ? get_sub_field('witlandingpages_phone_background_color') : 'white' ;
+        $wlp_phone_background_hvr = (get_sub_field('witlandingpages_phone_background_color_hover')) ? get_sub_field('witlandingpages_phone_background_color_hover') : '' ;
+        $wlp_phone_border_radius = (get_sub_field('witlandingpages_phone_border_radius')) ? get_sub_field('witlandingpages_phone_border_radius') : '1' ;
+        $wlp_phone_body_color = (get_sub_field('witlandingpages_phone_body_color')) ? get_sub_field('witlandingpages_phone_body_color') : 'black' ;
         $wlp_phone_body_color_hover = (get_sub_field('witlandingpages_phone_body_color_hover')) ? get_sub_field('witlandingpages_phone_body_color_hover') : '' ;
-        // $wlp_phone_if_custom = (get_sub_field('witlandingpages_phone_custom_icon')) == '1' ? 'customPhone' : 'defaultPhone' ;
         $wlp_phone_default_filter = (get_sub_field('witlandingpages_phone_default_icon_filter')) ? get_sub_field('witlandingpages_phone_default_icon_filter') : '' ;
-        // $wlp_phone_png_svg = (get_sub_field('witlandingpages_phone_png_svg')) == 'PNG' ? 'pngPhone' : 'svgPhone' ;
-        // $wlp_phone_png = (get_sub_field('witlandingpages_phone_png')) ? get_sub_field('witlandingpages_phone_png')['url'] : '' ;
-        // $wlp_phone_png_hover = (get_sub_field('witlandingpages_phone_png_hover')) ? get_sub_field('witlandingpages_phone_png_hover')['url'] : '' ;
-        // $wlp_phone_svg_filter = (get_sub_field('witlandingpages_phone_custom_icon_filter')) ? get_sub_field('witlandingpages_phone_custom_icon_filter') : '' ;
-        // $wlp_phone_svg_filter_hover = (get_sub_field('witlandingpages_phone_custom_icon_filter_hover')) ? get_sub_field('witlandingpages_phone_custom_icon_filter_hover') : '' ;
-        // $wlp_phone_svg = (get_sub_field('witlandingpages_phone_svg')) ? get_sub_field('witlandingpages_phone_svg')['url'] : '' ;
+        $wlp_phone_default_filter_hvr = (get_sub_field('witlandingpages_phone_default_filter_hover')) ? get_sub_field('witlandingpages_phone_default_filter_hover') : '' ;
     
         endwhile;
      endif; 
 
      // <style> variables:
         $wlp_styles_phone_bkgnd = "background-color: " . $wlp_phone_background . ";";
+        $wlp_styles_phone_bkgnd_hvr = "background-color: " . $wlp_phone_background_hvr . ";";
         $wlp_styles_phone_rad = "border-radius: " . $wlp_phone_border_radius . "rem;";
-        $wlp_styles_phone_loc_color = "color: " . $wlp_phone_location_color . ";";
         $wlp_styles_phone_body_color = "color: " . $wlp_phone_body_color . ";";
         $wlp_styles_pbc_hover = "color: " . $wlp_phone_body_color_hover . ";";
         $wlp_styles_phone_std_filter = "filter: " . $wlp_phone_default_filter; // DO NOT include extra semi-colon here. Field value brings it over.
-        // $wlp_styles_phone_custom_filter = "filter: " . $wlp_phone_svg_filter; // DO NOT include extra semi-colon here. Field value brings it over.
-        // $wlp_styles_pcf_hover = "filter: " . $wlp_phone_svg_filter_hover; // DO NOT include extra semi-colon here. Field value brings it over.
-    
-        // $wlp_phone_style_png = 'background: url(' . $wlp_phone_png . ') center / cover;';
-        // $wlp_phone_style_png_hover = 'background: url(' . $wlp_phone_png_hover . ') center / cover;';
-        // $wlp_phone_style_svg = 'background: url(' . $wlp_phone_svg . ') center / cover;';
+        $wlp_styles_phone_std_hvr = "filter: " . $wlp_phone_default_filter_hvr; // DO NOT include extra semi-colon here. Field value brings it over.
 
 
     $wlp_phonedisplay_div = 
         '<style>
-        .phoneBtnItemLocation {' .  $wlp_styles_phone_loc_color . '}
         .phoneBtn {' . $wlp_styles_phone_bkgnd . $wlp_styles_phone_rad . '}
+        .phoneBtn:hover {' . $wlp_styles_phone_bkgnd_hvr . '}
         .phoneBtnBody {' . $wlp_styles_phone_body_color . '}
         .phoneBtnBody:hover {' . $wlp_styles_pbc_hover . '}
+        .iconImageFilter {' . $wlp_styles_phone_std_filter . '}
+        .iconImageFilter:hover {' . $wlp_styles_phone_std_hvr . '}
         </style>
 
         <div class="flex items-center justify-center mx-auto my-0 phoneBtns flex-nowrap max-w-340">
             <div class="phoneBtnItem mb-2.5">
-                <span class="phoneBtnItemLocation block mb-1.5 font-bold">' . $wlp_phone_1_location . '</span>
                 <a href="tel:' . $wlp_phone_1_href . ' " class="px-3 py-2 text-base font-bold phoneBtn btn"><img class="inline-block mr-1 w-3.5 align-middle iconImageFilter" src="' . $wlp_call_button_icon . ' " /> <span class="phoneBtnBody">' . $wlp_phone_1_display . '</span></a>
             </div>
 
@@ -71,10 +60,14 @@ function witlandingpages_phone_button( $atts ) {
 
         return $wlp_phonedisplay_div;
 };
-add_shortcode('witlandingpages_phone_button', 'witlandingpages_phone_button');
+add_shortcode('witlandingpages_default_phone', 'witlandingpages_default_phone');
+
+// Phone Button (with ONE phone number and PNG icons):
 
 
-// Phone Button (with TWO phone numbers):
+// Phone Button (with ONE phone number and SVG icon):
+
+// Phone Button (with TWO phone numbers and default icon):
 
 function witlandingpages_twophones_button( $atts ) {
     extract(shortcode_atts(array(
@@ -110,7 +103,11 @@ function witlandingpages_twophones_button( $atts ) {
 };
 add_shortcode('witlandingpages_twophones_button', 'witlandingpages_twophones_button');
 
-// Request Service button:
+// Phone Button (with TWO phone numbers and PNG icons):
+
+// Phone Button (with TWO phone numbers and SVG icon):
+
+// Request Service button (with default icon):
 
 function witlandingpages_request_button( $atts ) {
     extract(shortcode_atts(array(
@@ -130,6 +127,10 @@ function witlandingpages_request_button( $atts ) {
 
 };
 add_shortcode('witlandingpages_request_button', 'witlandingpages_request_button');
+
+// Request Service button (with PNG icons):
+
+// Request Service button (with SVG icon):
 
 
 
