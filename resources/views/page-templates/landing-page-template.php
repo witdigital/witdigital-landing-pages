9f8619322f7090ebff0e2040081f8cff129d7f34
Erin Dawson <erinedawson@gmail.com>
2021-10-06 15:07:11 -0400

    add main wrapper

--- resources/views/page-templates/landing-page-template.php
+++ resources/views/page-templates/landing-page-template.php
@@ -18,23 +18,28 @@ get_header(); ?>
     <?php the_content() ?>
 	<?php endwhile;  ?> -->
 
-<div class="wit-landing-page container max-w-full mx-auto py-4"> <!-- Start of .wit-landing-page -->
+<!-- Must wrap div in element 'main' to pull in typography-main.scss from theme -->    
+<main class="main">
 
-<?php include_once('service-page-modules/hero.php') ?>
-<?php include_once('service-page-modules/intro.php') ?>
-<?php include_once('service-page-modules/about.php') ?>
-<?php include_once('service-page-modules/cta.php') ?>
-<?php include_once('service-page-modules/guarantees.php') ?>
+    <div class="container max-w-full py-4 mx-auto wit-landing-page"> <!-- Start of .wit-landing-page -->
 
-<?php include_once('service-page-modules/content.php') ?>
+    <?php include_once('service-page-modules/hero.php') ?>
+    <?php include_once('service-page-modules/intro.php') ?>
+    <?php include_once('service-page-modules/about.php') ?>
+    <?php include_once('service-page-modules/cta.php') ?>
+    <?php include_once('service-page-modules/guarantees.php') ?>
+
+    <?php include_once('service-page-modules/content.php') ?>
 
 
-<?php include('service-page-modules/testimonials.php') ?>      
+    <?php include('service-page-modules/testimonials.php') ?>      
 
 
-   
+    
 
-</div> <!-- End of .wit-landing-page -->
+    </div> <!-- End of .wit-landing-page -->
+
+</main>
 
 
 <?php get_footer(); ?>
