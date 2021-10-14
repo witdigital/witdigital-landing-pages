d4c8364aa140de8475033741ecab1c44f53c063a
Erin Dawson <erinedawson@gmail.com>
2021-10-14 18:12:18 -0400

    hide on-page items

---
+++ resources/views/page-templates/service-page-modules/hidden-elements.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Get values from "Page Options - Hide Elements":
+
+if( have_rows('witlandingpages_elements_to_hide') ):
+    while( have_rows('witlandingpages_elements_to_hide') ):the_row();
+
+    $test_var = (get_sub_field('witlandingpages_hidden_element')) ? get_sub_field('witlandingpages_hidden_element') : '' ; ?>
+
+<script>
+   
+    jQuery(document).ready(function($){
+    $("<?php echo $test_var ?>").hide();
+
+    });
+
+</script>
+    
+
+
+<?php endwhile;
+
+endif;
+
+
+?>
+
