4279b6c666a4a5a985eaf30e8f53b7ad8fc5d2c0
Ryan Mitchell <ryanmitchell@gmail.com>
2021-11-02 14:14:31 -0600

    minor code cleanup - extra lines

--- witdigital-landing-pages.php
+++ witdigital-landing-pages.php
@@ -12,15 +12,14 @@ Text Domain: witlandingpages
 /*  ==========================================================================
 	Check for composer and load it or disable the plugin
 	========================================================================== */
-
 namespace witlandingpages;
-if ( file_exists( plugin_dir_path( __FILE__ ) . '/vendor/autoload.php' ) ) {
-	require plugin_dir_path( __FILE__ ) . '/vendor/autoload.php';
+if (file_exists(plugin_dir_path(__FILE__).'/vendor/autoload.php')) {
+	require plugin_dir_path(__FILE__).'/vendor/autoload.php';
 } else {
 
 	function deactivate_plugin_conditional() {
 
-		deactivate_plugins( plugin_dir_path( __FILE__ ) . 'index.php' );
+		deactivate_plugins( plugin_dir_path(__FILE__).'index.php' );
 
 		echo '
 			<div class="notice notice-error is-dismissible">
@@ -30,7 +29,6 @@ if ( file_exists( plugin_dir_path( __FILE__ ) . '/vendor/autoload.php' ) ) {
 
 
 	}
-
 	add_action( 'admin_init', 'witlandingpages\deactivate_plugin_conditional' );
 
 }
@@ -38,9 +36,9 @@ if ( file_exists( plugin_dir_path( __FILE__ ) . '/vendor/autoload.php' ) ) {
 /* ==========================================================================
    Include ACF Pro if not ACF does not already exist on the site
    ========================================================================== */
-if ( ! class_exists( 'ACF' ) ) :
-	if ( file_exists( plugin_dir_path( __FILE__ ) . 'vendor/advanced-custom-fields/advanced-custom-fields-pro/acf.php' ) ) :
-		require plugin_dir_path( __FILE__ ) . '/vendor/advanced-custom-fields/advanced-custom-fields-pro/acf.php';
+if( ! class_exists('ACF') ) :
+	if (file_exists(plugin_dir_path(__FILE__).'vendor/advanced-custom-fields/advanced-custom-fields-pro/acf.php')) :
+		require plugin_dir_path(__FILE__).'/vendor/advanced-custom-fields/advanced-custom-fields-pro/acf.php';
 	endif;
 endif;
 /* ==========================================================================
@@ -54,16 +52,20 @@ $MyUpdateChecker = \Puc_v4_Factory::buildUpdateChecker(
 );
 
 // pull in ACF JSON
-if ( file_exists( plugin_dir_path( __FILE__ ) . 'app/acf_json.php' ) ) {
-	include plugin_dir_path( __FILE__ ) . 'app/acf_json.php';
+if( file_exists(plugin_dir_path(__FILE__) . 'app/acf_json.php') ) {
+	include plugin_dir_path(__FILE__) . 'app/acf_json.php';
 }
 
 /* ==========================================================================
    Bring in Setup
    ========================================================================== */
-if ( file_exists( plugin_dir_path( __FILE__ ) . 'app/setup.php' ) ) {
-	include plugin_dir_path( __FILE__ ) . 'app/setup.php';
+if( file_exists(plugin_dir_path(__FILE__) . 'app/setup.php') ) {
+	include plugin_dir_path(__FILE__) . 'app/setup.php';
 }
+
+
+
+
 
 
 /* ==========================================================================
@@ -77,16 +79,16 @@ if ( file_exists( plugin_dir_path( __FILE__ ) . 'app/setup.php' ) ) {
    Bring in Page Templates
    ========================================================================== */
 
-if ( file_exists( plugin_dir_path( __FILE__ ) . 'app/include-page-templates.php' ) ) {
-	include plugin_dir_path( __FILE__ ) . 'app/include-page-templates.php';
+if( file_exists(plugin_dir_path(__FILE__) . 'app/include-page-templates.php') ) {
+	include plugin_dir_path(__FILE__) . 'app/include-page-templates.php';
 }
 
 /* ==========================================================================
    Bring in Shortcodes
    ========================================================================== */
 
-if ( file_exists( plugin_dir_path( __FILE__ ) . 'app/shortcode.php' ) ) {
-	include plugin_dir_path( __FILE__ ) . 'app/shortcode.php';
+if( file_exists(plugin_dir_path(__FILE__) . 'app/shortcode.php') ) {
+	include plugin_dir_path(__FILE__) . 'app/shortcode.php';
 }
 // Flickers the least so far:
 // add_action('get_footer', 'witlandingpages\wlp_hidden_elements');
@@ -108,45 +110,45 @@ if ( file_exists( plugin_dir_path( __FILE__ ) . 'app/shortcode.php' ) ) {
 
 add_filter( 'the_content', 'witlandingpages\insert_service_content' );
 
-function insert_service_content( $content ) {
+function insert_service_content($content) {
 	// define variables:
-	$wlp_is_landing_page      = ( get_field( 'witlandingpages_is_landing_page' ) ) ? get_field( 'witlandingpages_is_landing_page' ) : '';
-	$wlp_show_hero            = ( get_field( 'witlandingpages_show_landing_page_hero' ) ) ? get_field( 'witlandingpages_show_landing_page_hero' ) : '';
-	$wlp_show_intro           = ( get_field( 'witlandingpages_show_landing_page_intro' ) ) ? get_field( 'witlandingpages_show_landing_page_intro' ) : '';
-	$wlp_show_content_sidebar = ( get_field( 'witlandingpages_show_lp_content_with_sidebar' ) ) ? get_field( 'witlandingpages_show_lp_content_with_sidebar' ) : '';
-	$wlp_show_cta             = ( get_field( 'witlandingpages_show_cta_section' ) ) ? get_field( 'witlandingpages_show_cta_section' ) : '';
-	$wlp_show_panels          = ( get_field( 'witlandingpages_show_panels_section' ) ) ? get_field( 'witlandingpages_show_panels_section' ) : '';
-	$wlp_show_accordions      = ( get_field( 'witlandingpages_show_accordion_section' ) ) ? get_field( 'witlandingpages_show_accordion_section' ) : '';
-	$wlp_show_testimonials    = ( get_field( 'witlandingpages_show_testimonials' ) ) ? get_field( 'witlandingpages_show_testimonials' ) : '';
-	$wlp_hide_elements        = ( get_field( 'witlandingpages_hide_page_elements' ) ) ? get_field( 'witlandingpages_hide_page_elements' ) : '';
+	$wlp_is_landing_page = (get_field('witlandingpages_is_landing_page')) ? get_field('witlandingpages_is_landing_page') : '' ;
+	$wlp_show_hero = (get_field('witlandingpages_show_landing_page_hero')) ? get_field('witlandingpages_show_landing_page_hero') : '' ;
+	$wlp_show_intro = (get_field('witlandingpages_show_landing_page_intro')) ? get_field('witlandingpages_show_landing_page_intro') : '' ;
+	$wlp_show_content_sidebar = (get_field('witlandingpages_show_lp_content_with_sidebar')) ? get_field('witlandingpages_show_lp_content_with_sidebar') : '' ;
+	$wlp_show_cta = (get_field('witlandingpages_show_cta_section')) ? get_field('witlandingpages_show_cta_section') : '' ;
+	$wlp_show_panels = (get_field('witlandingpages_show_panels_section')) ? get_field('witlandingpages_show_panels_section') : '' ;
+	$wlp_show_accordions = (get_field('witlandingpages_show_accordion_section')) ? get_field('witlandingpages_show_accordion_section') : '' ;
+	$wlp_show_testimonials = (get_field('witlandingpages_show_testimonials')) ? get_field('witlandingpages_show_testimonials') : '' ;
+	$wlp_hide_elements = (get_field('witlandingpages_hide_page_elements')) ? get_field('witlandingpages_hide_page_elements') : '' ;
 
 	// have to use conditional statement in order to have the hidden-elements.php script fire after jQuery has loaded for the site. it does not appear to matter which priority is given to the hook (re: script firing)
 
-	if ( in_the_loop() && is_main_query() && $wlp_is_landing_page ) {
+	if( in_the_loop() && is_main_query() && $wlp_is_landing_page ) {
 		echo '<div class="wlp-landing-page-inner">';
-		if ( $wlp_hide_elements ) {
-			include_once( 'resources/views/page-templates/service-page-modules/hidden-elements.php' );
+		if( $wlp_hide_elements ) {
+			include_once('resources/views/page-templates/service-page-modules/hidden-elements.php');
 		}
-		if ( $wlp_show_hero ) {
-			include_once( 'resources/views/page-templates/service-page-modules/hero.php' );
+		if( $wlp_show_hero ) {
+			include_once('resources/views/page-templates/service-page-modules/hero.php');
 		}
-		if ( $wlp_show_intro ) {
-			include_once( 'resources/views/page-templates/service-page-modules/intro.php' );
+		if( $wlp_show_intro ) {
+			include_once('resources/views/page-templates/service-page-modules/intro.php');
 		}
-		if ( $wlp_show_content_sidebar ) {
-			include_once( 'resources/views/page-templates/service-page-modules/about.php' );
+		if( $wlp_show_content_sidebar ) {
+			include_once('resources/views/page-templates/service-page-modules/about.php');
 		}
-		if ( $wlp_show_cta ) {
-			include_once( 'resources/views/page-templates/service-page-modules/cta.php' );
+		if( $wlp_show_cta ) {
+			include_once('resources/views/page-templates/service-page-modules/cta.php');
 		}
-		if ( $wlp_show_panels ) {
-			include_once( 'resources/views/page-templates/service-page-modules/guarantees.php' );
+		if( $wlp_show_panels ) {
+			include_once('resources/views/page-templates/service-page-modules/guarantees.php');
 		}
-		if ( $wlp_show_accordions ) {
-			include_once( 'resources/views/page-templates/service-page-modules/content.php' );
+		if( $wlp_show_accordions ) {
+			include_once('resources/views/page-templates/service-page-modules/content.php');
 		}
-		if ( $wlp_show_testimonials ) {
-			include_once( 'resources/views/page-templates/service-page-modules/testimonials.php' );
+		if( $wlp_show_testimonials ) {
+			include_once('resources/views/page-templates/service-page-modules/testimonials.php');
 		}
 		echo '</div>';
 	} else {
@@ -162,11 +164,10 @@ function add_landingpage_class( $classes ) {
 		'wlp-landing-page' => 'wlp-landing-page',
 	);
 
-	if ( get_field( 'witlandingpages_is_landing_page' ) ) {
-		foreach ( $include as $class => $do_include ) {
-			if ( $do_include ) {
-				$classes[ $class ] = $class;
-			}
+	if(get_field('witlandingpages_is_landing_page')) {
+		foreach ( $include as $class => $do_include )
+		{
+			if ( $do_include ) $classes[ $class ] = $class;
 		}
 
 		return $classes;
