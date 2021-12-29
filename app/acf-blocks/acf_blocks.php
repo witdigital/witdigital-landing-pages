607e3a41ce6f880c49b61d143d94ff7a61aadcaa
Erin Dawson <erinedawson@gmail.com>
2021-12-29 17:33:28 -0500

    register Intro Block

--- app/acf-blocks/acf_blocks.php
+++ app/acf-blocks/acf_blocks.php
@@ -10,3 +10,7 @@ $witcom_breakpoint_md = '1024px';
 if( file_exists(plugin_dir_path(__FILE__) . 'wlp-hero-block/register_block.php') ) {
 	include plugin_dir_path(__FILE__) . 'wlp-hero-block/register_block.php';
 }
+
+if( file_exists(plugin_dir_path(__FILE__) . 'wlp-intro-block/register_block.php') ) {
+	include plugin_dir_path(__FILE__) . 'wlp-intro-block/register_block.php';
+}
