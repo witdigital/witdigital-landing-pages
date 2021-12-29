9cdaa145d3bf87a90d938a7c816a9bd3f7c97639
Erin Dawson <erinedawson@gmail.com>
2021-12-29 14:56:23 -0500

    acf vars

--- app/acf-blocks/wlp-hero-block/views/render_template.php
+++ app/acf-blocks/wlp-hero-block/views/render_template.php
@@ -53,27 +53,21 @@ if ( ! empty( $block['align'] ) ) {
 // Get the post ID for getting fields if pulling from a post
 //$id = get_the_id();
 
-if ( get_field( 'wlp_hero_block_header_content' ) ) :
-	$wlp_hero_block_header_content = get_field( 'wlp_hero_block_header_content' );
-else :
-	$wlp_hero_block_header_content = 'WLP Hero Block Header Section';
-endif;
-
 $wlp_hero_title = (get_field('witlandingpages_hero_title')) ? get_field('witlandingpages_hero_title') : '' ;
+$wlp_hero_subtitle = (get_field('witlandingpages_hero_subtitle')) ? get_field('witlandingpages_hero_subtitle') : '' ;
 $wlp_hero_image = (get_field('witlandingpages_hero_image')) ? get_field('witlandingpages_hero_image')['url'] : '' ;
+$wlp_hero_sc = (get_field('witlandingpages_hero_shortcode')) ? do_shortcode (get_field('witlandingpages_hero_shortcode')) : '' ;
+$wlp_hero_title_color = (get_field('witlandingpages_hero_title_color')) ? get_field('witlandingpages_hero_title_color') : '' ;
+$wlp_hero_subtitle_color = (get_field('witlandingpages_hero_subtitle_color')) ? get_field('witlandingpages_hero_subtitle_color') : '' ;
+$wlp_overlay_class = (get_field('witlandingpages_overlay')) == 'Color' ? 'colorOverlay' : 'gradientOverlay' ;
+$wlp_overlay_color = (get_field('witlandingpages_overlay_color')) ? get_field('witlandingpages_overlay_color') : '' ;
+$wlp_overlay_gradient = (get_field('witlandingpages_overlay_gradient')) ? get_field('witlandingpages_overlay_gradient') : '' ;
+$wlp_hero_css = (get_field('witlandingpages_hero_additional_css')) ? get_field('witlandingpages_hero_additional_css') : '' ;
 
-
-if ( get_field( 'wlp_hero_block_content' ) ) :
-	$wlp_hero_block_content = get_field( 'wlp_hero_block_content' );
-else :
-	$wlp_hero_block_content = 'WLP Hero Block Content Section';
-endif;
-
-if ( get_field( 'wlp_hero_block_footer_content' ) ) :
-	$wlp_hero_block_footer_content = get_field( 'wlp_hero_block_footer_content' );
-else :
-	$wlp_hero_block_footer_content = 'WLP Hero Block Footer Section';
-endif;
+$wlp_htc = "color: " . $wlp_hero_title_color .'!important;';
+$wlp_hstc = "color: " . $wlp_hero_subtitle_color .';';
+$wlp_over_color = "background: " . $wlp_overlay_color . '!important;';
+$wlp_gradient = "background: " . $wlp_overlay_gradient . '!important;';
 
 /*  ==========================================================================
     Define InnerBLocks Template
@@ -121,10 +115,10 @@ endif;
 
             <div class="wlpHeroBlock_inner">
 
-                <div class="px-4 py-10 servicesHero hero laptop:pt-40 laptop:pb-24" style="background: url( <?php echo $witlandingpages_hero_image['url']; ?>) 100% 20% / cover;">
+                <div class="px-4 py-10 servicesHero hero laptop:pt-40 laptop:pb-24" style="background: url( <?php echo $wlp_hero_image; ?>) 100% 20% / cover;">
 	            <div class="altOverlay overlay <?php echo $wlp_overlay_class ?>"></div>
                     <div class="mx-auto text-center servicesHeroInner max-w-1000 laptop:pb-16 laptop:pt-6">
-                        <span class="mx-auto servicesHeroSubtitle max-w-450 laptop:px-3 laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $witlandingpages_hero_subtitle; ?></span>
+                        <span class="mx-auto servicesHeroSubtitle max-w-450 laptop:px-3 laptop:mb-3 text-20 laptop:text-20 text-green"><?php echo $wlp_hero_subtitle; ?></span>
                         <h1 class="leading-tight text-white servicesHeroTitle text-34 laptop:text-52 laptop:leading-none"><?php echo $wlp_hero_title ?></h1>
 
                         <div class="mt-3 text-center text-white servicesHeroCall laptop:mt-6">
