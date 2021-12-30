4eea12fc2a598e648165fb8e8f5f5c44cad8540a
Erin Dawson <erinedawson@gmail.com>
2021-12-30 14:03:38 -0500

    Testimonials block

--- app/acf-blocks/acf_blocks.php
+++ app/acf-blocks/acf_blocks.php
@@ -30,3 +30,7 @@ if( file_exists(plugin_dir_path(__FILE__) . 'wlp-panels-and-photo/register_block
 if( file_exists(plugin_dir_path(__FILE__) . 'wlp-bottom-content/register_block.php') ) {
 	include plugin_dir_path(__FILE__) . 'wlp-bottom-content/register_block.php';
 }
+
+if( file_exists(plugin_dir_path(__FILE__) . 'wlp-testimonials-block/register_block.php') ) {
+	include plugin_dir_path(__FILE__) . 'wlp-testimonials-block/register_block.php';
+}
