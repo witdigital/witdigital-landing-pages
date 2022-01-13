a234083722df6f66ddd4b7eb971614f689c752c7
Erin Dawson <erinedawson@gmail.com>
2022-01-13 18:55:28 -0500

    revert last commit

--- app/acf-blocks/wlp-content-with-sidebar/views/render_template.php
+++ app/acf-blocks/wlp-content-with-sidebar/views/render_template.php
@@ -44,7 +44,7 @@ if ( ! empty( $block['align'] ) ) {
 	Include block specific styling
 	========================================================================== */
 
-include_once 'block_styling.php';
+// include_once 'block_styling.php';
 
 /*  ==========================================================================
     Get ACF Values for Content
